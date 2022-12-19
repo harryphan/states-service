272f09fdcd6af1cbfa231b04f4ae82ffb7386891
Harry Phan <harryphan@gmail.com>
2022-12-19 09:20:53 -0500

    test fix

--- test/api.test.ts
+++ test/api.test.ts
@@ -2,22 +2,12 @@ import request from 'supertest';
 
 import app from '../src/app';
 
-describe('GET /api/v1', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/api/v1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: 'Testing',
-      }, done);
-  });
-});
+
 
 describe('GET /api/v1/states', () => {
   it('responds with a json message', (done) => {
     request(app)
-      .get('/api/v1/states')
+      .get('/v1/states')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, {states:['test']}, done);
